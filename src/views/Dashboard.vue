e6decad406cfe0732dd7ccd89be3abc27301f757
kleyla <kleyla01@live.com>
2020-11-13 15:48:59 -0400

    card, chip sort

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,32 +2,111 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-row>
-        <v-col xs="12" md="6">
-          <v-btn outlined block color="primary">BTN</v-btn>
-        </v-col>
-        <v-col xs="4" md="2">
-          <v-btn outlined block color="primary">BTN 2</v-btn>
-        </v-col>
-        <v-col xs="4" md="2">
-          <v-btn outlined block color="primary">BTN 2</v-btn>
-        </v-col>
-        <v-col xs="4" md="2">
-          <v-btn outlined block color="primary">BTN 2</v-btn>
-        </v-col>
+      <v-row class="mb-3">
+        <v-btn small text class="grey-text" @click="sortBy('title')">
+          <v-icon left small>mdi-folder</v-icon>
+          <span class="caption text-lowercase">by project name</span>
+        </v-btn>
+        <v-btn small text class="grey-text" @click="sortBy('person')">
+          <v-icon left small>mdi-account</v-icon>
+          <span class="caption text-lowercase">By person</span>
+        </v-btn>
       </v-row>
-      <v-row class="justify-center">
-        <v-col xs="4" md="3">
-          <v-btn outlined block color="success">BTN 1</v-btn>
-        </v-col>
-        <v-col xs="4" md="3">
-          <v-btn outlined block color="success">BTN 1</v-btn>
-        </v-col>
-      </v-row>
+      <v-card flat v-for="(project, index) in projects" :key="index">
+        <v-row class="justify-center" :class="`pa-3 project ${project.status}`">
+          <v-col cols="12" md="6">
+            <div class="caption grey--text">Project title</div>
+            <div>{{ project.title }}</div>
+          </v-col>
+          <v-col xs="5" sm="4" md="2">
+            <div class="caption grey--text">Person</div>
+            <div>{{ project.person }}</div>
+          </v-col>
+          <v-col xs="5" sm="4" md="2">
+            <div class="caption grey--text">Due by</div>
+            <div>{{ project.due }}</div>
+          </v-col>
+          <v-col xs="2" sm="4" md="2">
+            <div class="text-right">
+              <v-chip
+                small
+                :class="`${project.status} white--text caption my-2`"
+                >{{ project.status }}</v-chip
+              >
+            </div>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+      </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "Karen",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+        {
+          title: "Code up the homepage",
+          person: "Yolanda",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Andrea",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+        {
+          title: "Create a community forum",
+          person: "Rocio",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+      ],
+    };
+  },
+  methods: {
+    sortBy: function(prop) {
+      //   console.log(prop);
+      this.projects.sort((a, b) => (a[prop] < b[prop] ? -1 : 1) );
+    },
+  },
+};
 </script>
+<style scoped>
+.project.complete {
+  border-left: 4px solid #3cd1c2;
+}
+.project.ongoing {
+  border-left: 4px solid orange;
+}
+.project.overdue {
+  border-left: 4px solid tomato;
+}
+.v-chip.complete {
+  background: #3cd1c2 !important;
+}
+.v-chip.ongoing {
+  background: #ffaa2c !important;
+}
+.v-chip.overdue {
+  background: #f83e70 !important;
+}
+</style>
